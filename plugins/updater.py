6c967624b23a38538924d918fac58d3c549e3cb6
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-24 13:57:56 +0530

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -139,8 +139,7 @@ async def update(event, repo, ups_rem, ac_br):
 
 
 @ultroid_cmd(
-    pattern=r"update($| (now|deploy))",
-)
+    pattern=r"update($| (now|deploy))")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     conf = event.pattern_match.group(1).strip()
