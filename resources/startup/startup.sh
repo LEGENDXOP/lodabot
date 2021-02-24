3f8284d90bb542910289b0ddfd463ed21274ac0b
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-24 13:05:34 +0530

    Update startup.sh

--- resources/startup/startup.sh
+++ resources/startup/startup.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
- 
 echo "
  
         +-+ +-+ +-+ +-+ +-+ +-+ +-+
-        |U| |L| |T| |R| |O| |I| |D|
+                L O D A
         +-+ +-+ +-+ +-+ +-+ +-+ +-+
  
  
-      Visit @TheUltroid for updates!!
+      Visit @dclogger for updates!!
 
 "
  
